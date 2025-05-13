18b88fc127d288c8b1c1fe01f91f2b2423045d61
ahmedehab <ae413333@gmail.com>
2025-05-13 17:20:55 +0300

    added Image and Update Views

--- TechXpress.Data/obj/Debug/net9.0/TechXpress.Data.AssemblyInfo.cs
+++ TechXpress.Data/obj/Debug/net9.0/TechXpress.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechXpress.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f967bdda96bc9a63898da22b3be9cb1bb738b3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d936e66f2af11faad9586513058514e343db96")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechXpress.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechXpress.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
